e299042d95f617ed6c68e820c537707c3387cafe
Shafeeq Rabbani <shafeeqr2@gmail.com>
2017-11-26 02:05:53 -0500

    First commit. Code to add all digits until single digit remains.

---
+++ sumOfDigits.java
@@ -0,0 +1,36 @@
+import java.lang.Math; // headers MUST be above the first class
+
+public class sumOfDigits
+{
+ 
+  //add all digits
+  public static int sum (int num) {
+    int result = 0;
+    while (num > 0) {
+    	
+      result += num%10;
+      num = num / 10;
+      	
+    }
+    return result;
+  }
+  
+  public static int getSingleDigit(int num) {
+     
+    while (num > 9) {
+    	num = sum(num); 
+    }
+    
+    return num;
+  }
+
+  
+  public static void main(String[] args)
+  {
+
+    int num = 12345;
+    System.out.println(getSingleDigit(num));
+
+  }
+}
+
